b2538d9989dd59118ff7a4df72e8481cff375961
Juraj Repcik <yourigh.juraj@gmail.com>
2017-03-11 23:56:37 +0100

    Example C design

---
+++ example_AXI_usage.c
@@ -0,0 +1,19 @@
+
+rotary_val = *My_rotary; //read rotary outside of IF (clears on read), *My_rotary is register connected to IP core block
+		if (rotary_val)
+			{
+				if (rotary_val & 0x01) //switch rising edge
+				{
+					xil_printf("Switch pressed\n");
+				}
+				if ((rotary_val & 0x10)>>4) //right
+				{
+					count++;
+          xil_printf("count:%d\n",count);
+				}
+				if ((rotary_val & 0x20)>>5) //left
+				{
+					count--;
+          xil_printf("count:%d\n",count);
+				}
+			}
