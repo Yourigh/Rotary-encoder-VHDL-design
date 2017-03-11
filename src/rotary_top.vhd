49eb0bf5ab90b80a08c6ce872353c0ab40237fb1
Juraj Repcik <yourigh.juraj@gmail.com>
2017-03-11 23:39:19 +0100

    update

--- src/rotary_top.vhd
+++ src/rotary_top.vhd
@@ -8,14 +8,15 @@ use IEEE.STD_LOGIC_unsigned.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity rotary_top is
-    Port ( clk : in STD_LOGIC;
-           rotary_a : in STD_LOGIC;
-           rotary_b : in STD_LOGIC;
-           sw_in : in STD_LOGIC;
-           sw_out : out STD_LOGIC;
-		   UD_out : out STD_LOGIC_VECTOR (1 downto 0);
-           UD_latching_out : out STD_LOGIC_VECTOR (1 downto 0);
-		   clear_latch : in std_logic
+    Port ( clk              : in STD_LOGIC;
+           rotary_a         : in STD_LOGIC;
+           rotary_b         : in STD_LOGIC;
+           sw_in            : in STD_LOGIC;
+		       UD_out           : out STD_LOGIC_VECTOR (1 downto 0);
+           UD_latching_out  : out STD_LOGIC_VECTOR (1 downto 0);
+           SW_rise_latching : out STD_LOGIC;
+           SW_rise          : out STD_LOGIC;
+		       clear_latch      : in std_logic
            );
 end rotary_top;
 
@@ -24,26 +25,60 @@ architecture Behavioral of rotary_top is
 signal rotary_a_deb : STD_LOGIC := '0';
 signal rotary_b_deb : STD_LOGIC := '0';
 signal updown_out : STD_LOGIC_VECTOR (1 downto 0);
+signal AB_deb: STD_LOGIC_VECTOR (1 downto 0);
 signal UD_latching_d, UD_latching_q : STD_LOGIC_VECTOR (1 downto 0);
+signal sw_deb,sw_deb_delay 	: std_logic;
+signal SW_rise_latch_q,SW_rise_latch_d 	: std_logic;
+
+--inverted signals
+signal n_sw_in  : std_logic;
+signal n_rotary_a  : std_logic;
+signal n_rotary_b  : std_logic;
 
 begin
+n_sw_in <= not sw_in;
+n_rotary_a <= not rotary_a;
+n_rotary_b <= not rotary_b;
+AB_deb <= rotary_a_deb & rotary_b_deb;
+
 debsw: entity work.debounce
     generic map (counter_size => 20) --10.5ms
-	port map(clk => clk , button => not(sw_in) , result => sw_out);
+	port map(clk => clk , button =>  n_sw_in, result => sw_deb);
 deba: entity work.debounce
     generic map (counter_size => 15) --327us
-	port map(clk => clk , button => not(rotary_a) , result => rotary_a_deb);
+	port map(clk => clk , button =>  n_rotary_a, result => rotary_a_deb);
 debb: entity work.debounce
     generic map (counter_size => 15) --327us
-	port map(clk => clk , button => not(rotary_b) , result => rotary_b_deb);
+	port map(clk => clk , button =>  n_rotary_b, result => rotary_b_deb);
 enc:entity work.x1enc2
-	port map(AB => rotary_a_deb & rotary_b_deb, Clk => clk , UD => updown_out);
-	
+	port map(AB => AB_deb, Clk => clk , UD => updown_out);
+
+swrise: process(clk)
+begin
+   if rising_edge(clk) then
+       sw_deb_delay <= sw_deb;
+       SW_rise <= '0';
+       SW_rise_latch_q <= SW_rise_latch_d; --remember
+       if (((sw_deb_delay xor sw_deb) and sw_deb)='1') then
+           --rising edge detector for switch
+           SW_rise <= '1';
+           SW_rise_latch_q <= '1';
+       end if;
+       if (clear_latch = '1') then
+   			SW_rise_latch_q <= '0';
+   		 end if;
+   end if;
+end process swrise;
+SW_rise_latch_d <= SW_rise_latch_q;
+SW_rise_latching <= SW_rise_latch_q;
+
 UD_latching_out <= UD_latching_q;
 UD_out <= updown_out;
 lch:process(clk)
 begin
+
 	if rising_edge(clk) then
+    UD_latching_q <= UD_latching_d;
 		if (updown_out>"00") then
 			UD_latching_q <= updown_out;
 		end if;
--- src/x4enc2_tb.vhd
+++ src/x4enc2_tb.vhd
@@ -6,61 +6,68 @@
 -- Design Name:   Encoder x 4 (design B) Tesebench
 -- Module Name:   D:/Dewwork/Dew2012/Active/research/hdl/encoder/vhdl/x4enc2/TB1.vhd
 -- Project Name:  x4enc2
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
+-- Target Device:
+-- Tool versions:
+-- Description:
+--
 -- VHDL Test Bench Created by ISE for module: x4enc2
--- 
+--
 -- Dependencies:
--- 
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
 --
--- Notes: 
+-- Notes:
 -- This testbench has been automatically generated using types std_logic and
 -- std_logic_vector for the ports of the unit under test.  Xilinx recommends
 -- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
+-- to guarantee that the testbench will bind correctly to the post-implementation
 -- simulation model.
 --------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
+
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --USE ieee.numeric_std.ALL;
- 
+
 ENTITY TB1 IS
 END TB1;
- 
-ARCHITECTURE behavior OF TB1 IS 
- 
+
+ARCHITECTURE behavior OF TB1 IS
+
 
    --Inputs
    signal AB : std_logic_vector(1 downto 0) := (others => '0');
    signal Clk : std_logic := '0';
    signal clear_latch : std_logic := '0';
+   signal rot_SW : std_logic := '0';
 
  	--Outputs
    signal UD : std_logic_vector(1 downto 0);
+   signal UD_ce : std_logic_vector(1 downto 0);
+   signal SW_rise_sig : std_logic;
+   signal slv_reg0 : std_logic_vector(5 downto 0);
 
    -- Clock period definitions
    constant Clk_period : time := 10 ns;
- 
+
 BEGIN
- 
+
 	-- Instantiate the Unit Under Test (UUT)
-   uut: entity work.rotary_top PORT MAP (
-          clk => Clk,
-          count_out => open,
-          rotary_a => AB(0),
-          rotary_b => AB(1),
-          UD_out => open,
-          UD_latching_out => UD,
-          clear_latch =>     clear_latch   
-        );
+   uut: entity work.rotary_top
+        port map(
+            clk        =>       Clk,
+            rotary_a   =>       AB(0),
+            rotary_b   =>       AB(1),
+            sw_in      =>       rot_SW,
+            UD_out      =>      UD_ce,
+            UD_latching_out  => slv_reg0(5 downto 4),
+            SW_rise_latching => slv_reg0(0),
+            SW_rise     =>       SW_rise_sig,
+            clear_latch      => clear_latch --reading strobe clears
+            );
 
    -- Clock process definitions
    Clk_process :process
@@ -70,57 +77,65 @@ BEGIN
 		Clk <= '1';
 		wait for Clk_period/2;
    end process;
- 
+
 
    -- Stimulus process
    stim_proc: process
-   begin		
-       -- hold reset state for 100 ns.
-      wait for 100 ns;	
+   begin
+       -- hold reset state for 1000 ns.
+      wait for 1 ms;
     clear_latch <= '0';
       wait for Clk_period*5;
 
-      -- insert stimulus here 
+      -- insert stimulus here
 		-- CW direction: A leads B 90 degree
 		AB <= "10";
-		wait for 100 ns;
+		wait for 1 ms;
 		AB <= "11";
-		wait for 100 ns;
+		wait for 1 ms;
 		AB <= "01";
-		wait for 100 ns;
+		wait for 1 ms;
 		AB <= "00";
-		wait for 200 ns;
-        clear_latch <= '1';
-        wait for 40 ns;
-        clear_latch <= '0';
+    rot_SW <= '1';
+		wait for 1 ms;
+    clear_latch <= '1';
+    wait for 40 ns;
+
+    clear_latch <= '0';
 		AB <= "10";
-		wait for 100 ns;
+		wait for 1 ms;
 		AB <= "11";
-		wait for 100 ns;
+		wait for 1 ms;
 		AB <= "01";
-		wait for 100 ns;
+		wait for 1 ms;
 		AB <= "00";
-		wait for 400 ns;
+		wait for 4 ms;
 		clear_latch <= '1';
 		wait for 10 ns;
 		clear_latch <= '0';
 		-- CCW direction: B leads A 90 degree
 		AB <= "01";
-		wait for 100 ns;
+		wait for 1 ms;
 		AB <= "11";
-		wait for 100 ns;
+		wait for 1 ms;
 		AB <= "10";
-		wait for 100 ns;
+		wait for 1 ms;
+    rot_SW <= '0';
 		AB <= "00";
-		wait for 100 ns;
+		wait for 1 ms;
 		AB <= "01";
-		wait for 100 ns;
+		wait for 1 ms;
 		AB <= "11";
-		wait for 100 ns;
+		wait for 1 ms;
 		AB <= "10";
-		wait for 100 ns;
+		wait for 1 ms;
 		AB <= "00";
-
+    wait for 5 ms;
+    rot_SW <= '0';
+    wait for 5 ms;
+    clear_latch <= '1';
+    wait for 10 ns;
+    clear_latch <= '0';
       wait;
    end process;
 
